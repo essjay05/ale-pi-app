b25f4f3e3a34ada6e186638f8719e2f444ea6471
Joy Serquina <Joy.Serquina@scorpion.co>
2020-09-13 15:16:59 -0700

    Created components: beers-list, create-beer, edit-beer, imported beers-list to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import BeersList from './components/beers-list.component';
 
+import logo from "./logo.png";
 
 class App extends Component {
   render () {
     return (
       <Router>
-        <Route path="/" exact component="{BeersList}" />
-        <Route path="/edit:id" component="{EditBeer}" />
-        <Route path="/create" compoenent="CreateBeer}" />
+        
         <div className="container">
           <h2>MERN-Stack Ale-PI App</h2>
         </div>
+        <Route path="/" exact component="{BeersList}" />
+        <Route path="/edit:id" component="{EditBeer}" />
+        <Route path="/create" compoenent="CreateBeer}" />
       </Router>
     )
   }
---
+++ src/components/beers-list.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BeersList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Cheers!</h1>
+                <p>Welcome to Beers List Component!</p>
+            </div>
+        )
+    }
+}
